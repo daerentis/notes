a614f6283240bf46c6293e3049444bbbf8c3e93a
Christian Strausbach <christian.strausbach@freshlime.de>
2021-03-12 13:49:30 +0100

    Changed the IndexedDB library and added search functionality

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import "./node_modules/quill/dist/quill.bubble.css"
 import './styles.scss'
 
 import Vue from './node_modules/vue/dist/vue.min'
-import { createStore, entries, get, set, del } from 'idb-keyval'
+import Dexie from 'dexie';
+import lunr from 'lunr';
 import moment from 'moment';
 import 'moment/locale/de';
 import Quill from 'quill';
@@ -12,7 +13,10 @@ import { quillEditor } from 'vue-quill-editor'
 var date = new Date();
 var today = moment().format('L');
 
-const db = createStore('notes', 'notes');
+var db = new Dexie("notes2");
+db.version(1).stores({
+  notes: "++id, date, content",
+});
 
 new Vue({
   el: '#app',
@@ -20,6 +24,7 @@ new Vue({
     quillEditor
   },
   data: {
+    query: '',
     content: '',
     editorOption: {
       theme: 'bubble',
@@ -31,53 +36,103 @@ new Vue({
           userOnly: true
         },
         toolbar: [
-          ['bold', 'italic', 'underline'],        // toggled buttons
-          ['blockquote', 'code-block'],
-        
-          [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+          ['bold', 'italic', 'underline'],
+          ['blockquote', 'code-block'],        
+          [{ 'header': 1 }, { 'header': 2 }],
           [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-          [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-          ['clean'] 
+          [{ 'indent': '-1'}, { 'indent': '+1' }],
+          ['clean'],
           ['image', 'video'],
         ]         
       }
     },
     sidebar: [],
+    marked: [],
     today: today,
     active: today
   },
-  
+    
   mounted() {
-    get(today, db).then((note) => this.content = note);
     this.reloadNotes();
   },
   
-  methods: {
+  methods: {    
     onEditorReady(quill) {
-      get(today, db).then((val) => quill.setContents(val));
-      quill.focus();
+      db.notes.get({date: today}).then((note) => {
+        quill.setContents(note.content)
+        quill.focus();
+      });
     },
     
     onEditorChange({ quill, html, text }) {
-      set(this.active, quill.editor.delta, db);
+      db.transaction('r', db.notes, () => {
+        return db.notes.get({date: this.active});
+      }).then((note) => {
+        if (note == undefined)
+          db.notes.put({date: this.active, content: quill.editor.delta});
+        else
+          db.notes.update(note.id, { content: quill.editor.delta });
+      });
     },
     
-    reloadNotes(callback) {
-      entries(db).then((notes) => this.sidebar = notes.reverse());
+    reloadNotes() {
+      this.sidebar = [];
+      db.notes.each((note) => {
+        this.sidebar.push(note);
+      })
     },
     
-    selectNote(note) {
-      get(note, db).then((val) => this.$refs.quillEditor.quill.setContents(val));
-      this.active = note;
+    selectNote(date) {
+      db.notes.get({date: date}).then((note) => {
+        if (note == undefined)
+          this.$refs.quillEditor.quill.setContents('');
+        else
+          this.$refs.quillEditor.quill.setContents(note.content);
+      
+        this.active = date;
+      });
+      this.active = date;
+    },
+    
+    searchNote() {
+      var query = this.query;
+      var notes = [];
+      var sidebar = this.sidebar;
+      
+      if (query) {
+        db.notes.each((note)  => {
+          note.content = JSON.stringify(note.content).replace(/\{|\}|\[|\]|:|"|,|(\\n)|ops/gm, ' ');
+          notes.push(note);
+        }).then(() => {
+          var idx = lunr(function () {
+            this.field('date')
+            this.field('content')
+            
+            notes.forEach(function (note) {
+              this.add(note)
+            }, this);
+          })
+          return idx.search(query);
+        }).then((results) => {
+          if (results) {
+            this.marked = [];
+            this.sidebar.forEach((element) => {
+              results.forEach((result) => {
+                this.marked[element.id] = (element.id == result.ref)
+              })
+            })  
+          }
+        })
+     }
     },
     
     removeNote(note) {
       if (confirm("Are you sure you want to delete this note?") == true) {
-        del(note, db).then(() => {
+        db.notes.delete(note.id).then(() => {
           this.reloadNotes()
           
-          if (note == this.active)
-            this.active = today
+          if (note.date == this.active)
+            this.active = today;
         });
       }
     }
